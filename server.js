c4a589a85bf7b6c2a5cd6c3cb7d94284f4c27141
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-07-24 14:40:47 -0500

    Setup temporary database books

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 if (!PORT) throw new Error('PORT is missing');
 
 const express = require('express');
@@ -8,10 +8,36 @@ const cors = require('cors');
 
 const app = express();
 
+// Middleware
 app.use(cors());
 
 app.get('/', (request, response) => response.send('Server running'));
 
-app.get('/test', (request, response) => response.send('hello world'));
+// Temp Database
+let nextBookId = 1;
+const books = [
+  {
+    book_id: nextBookId++,
+    title: 'Harry Potter and the Sorcerer&apos;s Stone',
+    author: 'J.K. Rowling',
+    image_url: 'https://images-na.ssl-images-amazon.com/images/I/51HSkTKlauL._SX346_BO1,204,203,200_.jpg'
+  },
+  {
+    book_id: nextBookId++,
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+    image_url: 'https://images-na.ssl-images-amazon.com/images/I/51jNORv6nQL._SX340_BO1,204,203,200_.jpg'
+  },
+  {
+    book_id: nextBookId++,
+    title: 'Harry Potter and the Prisoner of Azkaban',
+    author: 'J.K. Rowling',
+    image_url: 'https://images-na.ssl-images-amazon.com/images/I/51JAGM6G3PL._SX316_BO1,204,203,200_.jpg'
+  },
+];
+
+app.get('/api/v1/books', (request, response) => {
+  response.send(books);
+});
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
